c9ddcf9ed57ea011a221bb8b14092bd7e0bbaa83
AdrianPascual142003 <adrianpascual142003@gmail.com>
2023-04-15 17:25:12 +0200

    [Add] Profile and Main Page

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { Observable, catchError, map, throwError } from 'rxjs';
+import { Photo } from '../shared/model/photo.interface';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+
+  constructor(
+    private httpClient: HttpClient,
+  ) { }
+
+  private apiURL = 'http://localhost/api/photo/';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  getAll(): Observable<Photo[]> {
+    return this.httpClient.get<Photo[]>(this.apiURL)
+    .pipe(
+      catchError(this.errorHandler)
+    );
+  }
+
+  create(photo: Photo): Observable<Photo> {
+    return this.httpClient.post<Photo>(this.apiURL, JSON.stringify(photo), this.httpOptions)
+    .pipe(
+      catchError(this.errorHandler)
+    );
+  }
+
+  find(id: number): Observable<Photo> {
+    return this.httpClient.get<Photo>(this.apiURL + id)
+    .pipe(
+      catchError(this.errorHandler)
+    );
+  }
+
+  update(user: Photo): Observable<Photo> {
+    return this.httpClient.put<Photo>(this.apiURL + user.id, JSON.stringify(user), this.httpOptions)
+    .pipe(catchError(this.errorHandler)
+    )
+  }
+
+  delete(id: number) {
+    return this.httpClient.delete(this.apiURL + id, this.httpOptions)
+    .pipe(catchError(this.errorHandler)
+    );
+  }
+
+  errorHandler(error: any) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(errorMessage);
+  }
+
+}
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { User } from '../shared/model/user.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  private apiURL = 'http://localhost/api/user/';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  getAll(): Observable<User[]> {
+    return this.httpClient.get<User[]>(this.apiURL)
+    .pipe(
+      catchError(this.errorHandler)
+    );
+  }
+
+  create(user: User): Observable<User> {
+    return this.httpClient.post<User>(this.apiURL, JSON.stringify(user), this.httpOptions)
+    .pipe(
+      catchError(this.errorHandler)
+    );
+  }
+
+  // TODO comprobar que no necesita pasarle la id
+  update(user: User): Observable<User> {
+    return this.httpClient.put<User>(this.apiURL + user.id, JSON.stringify(user), this.httpOptions)
+    .pipe(catchError(this.errorHandler)
+    )
+  }
+
+  find(id: number): Observable<User> {
+    return this.httpClient.get<User>(this.apiURL + id)
+    .pipe(
+      catchError(this.errorHandler)
+    )
+  }
+
+  delete(id: number) {
+    return this.httpClient.delete(this.apiURL + id, this.httpOptions)
+    .pipe(catchError(this.errorHandler)
+    );
+  }
+
+  errorHandler(error: any) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(errorMessage);
+  }
+
+}
